2a21b4b96f2b14b6f251873194c178f3de67ff27
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-07-29 16:24:30 +0200

    Second attempt

---
+++ nix/overlay.nix
@@ -0,0 +1,16 @@
+final:
+  previous:
+    with final.haskell.lib;
+    {
+      haskellPackages = previous.haskellPackages.override (old: {
+        overrides = final.lib.composeExtensions (old.overrides or (_: _: {})) (
+          self: super:
+            let smosPkg = name:
+                (self.callCabal2nix name (../. + "/${name}") {});
+            in final.lib.genAttrs [
+              "smos-data"
+              "smos-data-gen"
+            ] smosPkg
+        );
+      });
+    }
