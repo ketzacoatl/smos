32dae607a97c93cac5415e12910372df48912534
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-07-29 20:21:56 +0200

    Build more than just cursors

--- scripts/ci/script.sh
+++ scripts/ci/script.sh
@@ -8,6 +8,8 @@ case $BUILD_KIND in
   nix)
     nix-build -A haskellPackages.cursor
     nix-build -A haskellPackages.cursor-gen
+    nix-build -A haskellPackages.smos-data
+    nix-build -A haskellPackages.smos-data-gen
     ;;
   *)
     echo "Unknown build kind."
